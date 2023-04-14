556f127cb6b19e4ce402d5e8aa447e60a53cb5ae
fiction13 <fiction13@yandex.ru>
2023-04-14 11:31:59 +0300

    Reset to dev

--- tinkoff.php
+++ tinkoff.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * @package    AtomS Connect - Tinkoff
- * @version    1.1.0
+ * @version    __DEPLOY_VERSION__
  * @author     Atom-S - atom-s.com
  * @copyright  Copyright (c) 2017 - 2022 Atom-S LLC. All rights reserved.
  * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
